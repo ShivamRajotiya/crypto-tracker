d0d9170b17f3fe91d0f832fc2bfde2529b10d27b
Shivam <rajotiyashivam4@gmail.com>
2023-05-03 00:59:18 +0530

    footer news section added

--- src/config/api.js
+++ src/config/api.js
@@ -9,3 +9,7 @@ export const HistoricalChart = (id, days = 365, currency) =>
 
 export const TrendingCoins = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+
+  export const News = 
+    'https://newsdata.io/api/1/news?apikey=pub_214338b1c45c6ba72046ce6da75b3d5eb233c&q=cryptocurrency'
+  
